81efd48b68dccd949c812a2a3f38e4ac251c7a10
idist-hn <HanhHanh2508@>
2025-08-19 10:19:18 +0700

    brute force - update desc

--- sources/algorithms/brute-force.go
+++ sources/algorithms/brute-force.go
@@ -2,7 +2,7 @@ package algorithms
 
 // BruteForce tìm kiếm chuỗi con bằng thuật toán Brute Force
 // Độ phức tạp thời gian: O(n×m) trong trường hợp xấu nhất, O(n+m) trong trường hợp tốt nhất
-// Độ phức tạp không gian: O(1)
+// Độ phức tạp không gian: O(1), không sử dụng thêm bộ nhớ nào, chỉ sử dụng các biến cục bộ, không sử dụng đệ quy, không sử dụng cấu trúc dữ liệu, không sử dụng hàm gọi hàm
 // Trong đó n là độ dài của chuỗi source, m là độ dài của chuỗi target
 // Thuật toán Brute Force là thuật toán tìm kiếm chuỗi con đơn giản nhất, nó sẽ so sánh từng ký tự của chuỗi target với chuỗi source một cách tuần tự
 // Tìm giá trị của chuỗi target trong chuỗi source bằng thuật toán Brute Force, trả về true nếu tìm thấy, false nếu không tìm thấy
